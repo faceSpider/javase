7a1912ca51a270e987e97394980db42efbdc1697
xingma <532973699@qq.com>
2022-12-19 18:23:56 +0800

    第一次测试提交

---
+++ javase/src/day01/chengfabiao.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class chengfabiao {
+    public static void main(String[] args) {
+        for (int i = 1; i <10 ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(j+"*"+i+"="+j*i+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
